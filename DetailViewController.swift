41c1958df5b0e59e10769c1b850c7f74039be242
OnlyThen <onlythen@icloud.com>
2017-06-15 21:49:29 +0800

    Initial commit

---
+++ DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by 张瑜 on 2017/2/5.
+//  Copyright © 2017年 张瑜. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+        
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
